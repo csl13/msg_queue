189bca2f3a649fa71f44a91cf5c86d6830a8d747
csl13 <chensonglin8721@126.com>
2014-03-31 22:33:56 +0800

    mod worker.cpp

--- src/worker.cpp
+++ src/worker.cpp
@@ -9,49 +9,29 @@
 //  Task ventilator
 //  Binds PUSH socket to tcp://localhost:5557
 //  Sends batch of tasks to workers via that socket
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-#include <zmq.h>
 #include "zhelpers.h"
 
 int main (void)
 {
+    //  Socket to receive messages on
     void *context = zmq_ctx_new ();
+    void *receiver = zmq_socket (context, ZMQ_PULL);
+    zmq_connect (receiver, "tcp://localhost:5557");
 
-    //  Socket to send messages on
+    //  Socket to send messages to
     void *sender = zmq_socket (context, ZMQ_PUSH);
-    zmq_bind (sender, "tcp://*:5557");
+    zmq_connect (sender, "tcp://localhost:5558");
 
-    //  Socket to send start of batch message on
-    void *sink = zmq_socket (context, ZMQ_PUSH);
-    zmq_connect (sink, "tcp://localhost:5558");
-
-    printf ("Press Enter when the workers are ready: ");
-    getchar ();
-    printf ("Sending tasks to workers¡­\n");
-
-    //  The first message is "0" and signals start of batch
-    s_send (sink, "0");
-
-    //  Initialize random number generator
-    srandom ((unsigned) time (NULL));
-
-    //  Send 100 tasks
-    int task_nbr;
-    int total_msec = 0;     //  Total expected cost in msecs
-    for (task_nbr = 0; task_nbr < 100; task_nbr++) {
-        int workload;
-        //  Random workload from 1 to 100msecs
-        workload = randof (100) + 1;
-        total_msec += workload;
-        char string [10];
-        sprintf (string, "%d", workload);
-        s_send (sender, string);
+    //  Process tasks forever
+    while (1) {
+        char *string = s_recv (receiver);
+        printf ("%s.", string);     //  Show progress
+        fflush (stdout);
+        s_sleep (atoi (string));    //  Do the work
+        free (string);
+        s_send (sender, "");        //  Send results to sink
     }
-    printf ("Total expected cost: %d msec\n", total_msec);
-
-    zmq_close (sink);
+    zmq_close (receiver);
     zmq_close (sender);
     zmq_ctx_destroy (context);
     return 0;
